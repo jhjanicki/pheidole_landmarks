7c78798572397e2fb601adf16eb040436233f1a0
Julia Janicki <jhjanicki@gmail.com>
2017-11-30 14:43:38 +0900

    updated images and features

--- landmarks.js
+++ landmarks.js
@@ -3,314 +3,302 @@ var landmarks =
 [
   {
     "character": "1  R Post. Eye",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/1.PNG"
+    "img": "./img/1.PNG"
   },
   {
     "character": "2  R Ant. Eye",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/2.PNG"
+    "img": "./img/2.PNG"
   },
   {
     "character": "3  R Eye Apex",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/3.PNG"
+    "img": "./img/3.PNG"
   },
   {
     "character": "4  R Post. Antenna Socket (Under ridge in deepest part of hollow before it goes up again)",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/4.PNG"
+    "img": "./img/4.PNG"
   },
   {
     "character": "5  R Ant. Antenna Socket (Start of Ridge)",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/5.PNG"
+    "img": "./img/5.PNG"
   },
   {
     "character": "6  Clypeus Center Front",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/6.PNG"
+    "img": "./img/6.PNG"
   },
   {
     "character": "7  L Metastoma Margin? Corner",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/7.PNG"
+    "img": "./img/7.PNG"
   },
   {
     "character": "8  R Metastoma Margin? Corner",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/8.PNG"
+    "img": "./img/8.PNG"
   },
   {
     "character": "9  L Post. Eye",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/9.PNG"
+    "img": "./img/9.PNG"
   },
   {
     "character": "10 L Ant. Eye",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/10.PNG"
+    "img": "./img/10.PNG"
   },
   {
     "character": "11 L Eye Apex",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/11.PNG"
+    "img": "./img/11.PNG"
   },
   {
     "character": "12 L Ant. Antenna Socket (Start of Ridge)",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/12.PNG"
+    "img": "./img/12.PNG"
   },
   {
     "character": "13 L Post. Antenna Socket (Under ridge in deepest part of hollow before it goes up again)",
-    "part": "head", 
+    "part": "head",
     "index": 1,
-    "img":"./img/13.PNG"
+    "img": "./img/13.PNG"
   },
   {
     "character": "1 - Postero-Ventral Margin of Head on V Line",
-    "part": "head curve ventral line", 
+    "part": "head curve ventral line",
     "index": 2,
-    "img":"./img/C1-1.PNG"
+    "img": "./img/C1-1.PNG"
   },
   {
     "character": "2 - Antero-Ventral Margin of Head on V Line",
-    "part": "head curve ventral line", 
+    "part": "head curve ventral line",
     "index": 2,
-    "img":"./img/C1-2.PNG"
+    "img": "./img/C1-2.PNG"
   },
   {
     "character": "1 - L Post Margin of L Lat Line",
-    "part": "head curve L lateral line", 
+    "part": "head curve L lateral line",
     "index": 2,
-    "img":"./img/C2-1.PNG"
+    "img": "./img/C2-1.PNG"
   },
   {
     "character": "2 - L Ant Margin of L Lat Line",
-    "part": "head curve L lateral line", 
+    "part": "head curve L lateral line",
     "index": 2,
-    "img":"./img/C2-2.PNG"
+    "img": "./img/C2-2.PNG"
   },
   {
     "character": "1 - R Post Margin of R Lat Line",
-    "part": "head curve R lateral line", 
+    "part": "head curve R lateral line",
     "index": 2,
-    "img":"./img/C3-1.PNG"
+    "img": "./img/C3-1.PNG"
   },
   {
     "character": "2 - R Ant Margin of R Lat Line",
-    "part": "head curve R lateral line", 
+    "part": "head curve R lateral line",
     "index": 2,
-    "img":"./img/C3-2.PNG"
+    "img": "./img/C3-2.PNG"
   },
   {
     "character": "1 - Postero-Dorsal Margin of Head on D Line",
-    "part": "head curve dorsal line", 
+    "part": "head curve dorsal line",
     "index": 2,
-    "img":"./img/C4-1.PNG"
+    "img": "./img/C4-1.PNG"
   },
   {
     "character": "2 - POSTERIOR POINT OF CLYPEUS",
-    "part": "head curve dorsal line", 
+    "part": "head curve dorsal line",
     "index": 2,
-    "img":"./img/C4-2.PNG"
+    "img": "./img/C4-2.PNG"
   },
   {
-    "character": "14 Pron - Ant. Apex",
-    "part": "mesosoma", 
+    "character": "14 Pron - Median Antero-Dorsal Ridge Edge",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/14.PNG"
+    "img": "./img/14.PNG"
   },
   {
-    "character": "15 Pron - L A-L Corner meets Lat Ridge",
-    "part": "mesosoma", 
+    "character": "15 Pron - L A-L (Shoulder) Corner of pronotum - Set in D.view, then check vertical symmetry",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/15.PNG"
+    "img": "./img/15.PNG"
   },
   {
-    "character": "16 Pron - L Shoulder Corner",
-    "part": "mesosoma", 
+    "character": "16 Pron - L Apex of Antero-Ventral Pronotum Corner",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/16.PNG"
+    "img": "./img/16.PNG"
   },
   {
-    "character": "17 Pron - L Spiracle",
-    "part": "mesosoma", 
+    "character": "17 Pron - L Pronotal Ridge Apex",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/17.PNG"
+    "img": "./img/17.PNG"
   },
   {
-    "character": "18 Pron - L Posterior Corner",
-    "part": "mesosoma", 
+    "character": "18 Pron - L Spiracle",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/18.PNG"
+    "img": "./img/18.PNG"
   },
   {
-    "character": "19 ProMeso - Dorsal Apex",
-    "part": "mesosoma", 
+    "character": "19 Pron - L Posterior Corner",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/19.PNG"
+    "img": "./img/19.PNG"
   },
   {
-    "character": "20 Pron - R A-L Corner meets Lat Ridge",
-    "part": "mesosoma", 
+    "character": "20 ProMeso - Dorsal Apex",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/20.PNG"
+    "img": "./img/20.PNG"
   },
   {
-    "character": "21 Pron - R Shoulder Corner",
-    "part": "mesosoma", 
+    "character": "21 Pron - R A-L (Shoulder) Corner of pronotum - Set in D.view, then check vertical symmetry",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/21.PNG"
+    "img": "./img/21.PNG"
   },
   {
-    "character": "22 Pron - R Spiracle",
-    "part": "mesosoma", 
+    "character": "22 Pron - R Apex of Antero-Ventral Pronotum Corner",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/22.PNG"
+    "img": "./img/22.PNG"
   },
   {
-    "character": "23 Pron - R Posterior Corner",
-    "part": "mesosoma", 
+    "character": "23 Pron - R Pronotal Ridge Apex",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/23.PNG"
+    "img": "./img/23.PNG"
   },
   {
-    "character": "24 Center Metanotal Groove",
-    "part": "mesosoma", 
+    "character": "24 Pron - R Spiracle",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/24.PNG"
+    "img": "./img/24.PNG"
   },
   {
-    "character": "25 Meso - L A-L Corner",
-    "part": "mesosoma", 
+    "character": "25 Pron - R Posterior Corner",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/25.PNG"
+    "img": "./img/25.PNG"
   },
   {
-    "character": "26 Meso - L P-L Corner",
-    "part": "mesosoma", 
+    "character": "26 Center Metanotal Groove",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/26.PNG"
+    "img": "./img/26.PNG"
   },
   {
-    "character": "27 Meso - L Spiracle meets Groove",
-    "part": "mesosoma", 
+    "character": "27 Meso - L A-L Corner",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/27.PNG"
+    "img": "./img/27.PNG"
   },
   {
-    "character": "28 Meso - L Postero-Dorsal Corner (or Spine Tip)",
-    "part": "mesosoma", 
+    "character": "28 Meso - L P-L Corner",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/28.PNG"
+    "img": "./img/28.PNG"
   },
   {
-    "character": "29 Meso - R A-L Corner",
-    "part": "mesosoma", 
+    "character": "29 Meso - L Spiracle meets Groove",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/29.PNG"
+    "img": "./img/29.PNG"
   },
   {
-    "character": "30 Meso - R P-L Corner",
-    "part": "mesosoma", 
+    "character": "30 Meso - R A-L Corner",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/30.PNG"
+    "img": "./img/30.PNG"
   },
   {
-    "character": "31 Meso - R Spiracle meets Groove",
-    "part": "mesosoma", 
+    "character": "31 Meso - R P-L Corner",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/31.PNG"
+    "img": "./img/31.PNG"
   },
   {
-    "character": "32 Meso - R Postero-Dorsal Corner (or Spine Tip)",
-    "part": "mesosoma", 
+    "character": "32 Meso - R Spiracle meets Groove",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/32.PNG"
+    "img": "./img/32.PNG"
   },
   {
     "character": "33 Center Posterior Propodeum",
-    "part": "mesosoma", 
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/33.PNG"
+    "img": "./img/33.PNG"
   },
   {
-    "character": "34 Prop - L Spiracle",
-    "part": "mesosoma", 
+    "character": "34 Prop - L Spiracle (Ant.-most point)",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/34.PNG"
+    "img": "./img/34.PNG"
   },
   {
     "character": "35 Prop - L Post. Corner",
-    "part": "mesosoma", 
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/35.PNG"
+    "img": "./img/35.PNG"
   },
   {
     "character": "36 Prop - L Ant. Gland Opening",
-    "part": "mesosoma", 
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/36.PNG"
+    "img": "./img/36.PNG"
   },
   {
-    "character": "37 Prop - L Post. Gland Opening",
-    "part": "mesosoma", 
+    "character": "37 Prop - L Spine Tip",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/37.PNG"
+    "img": "./img/37.PNG"
   },
   {
-    "character": "38 Prop - L Spine Tip",
-    "part": "mesosoma", 
+    "character": "38 Prop - Center between Spines (or Ridge Apices)",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/38.PNG"
+    "img": "./img/38.PNG"
   },
   {
-    "character": "39 Prop - Center between Spines",
-    "part": "mesosoma", 
+    "character": "39 Prop - R Spine Tip",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/39.PNG"
+    "img": "./img/39.PNG"
   },
   {
-    "character": "40 Prop - R Spine Tip",
-    "part": "mesosoma", 
+    "character": "40 Prop - R Spiracle (Ant.-most point)",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/40.PNG"
+    "img": "./img/40.PNG"
   },
   {
-    "character": "41 Prop - R Spiracle",
-    "part": "mesosoma", 
+    "character": "41 Prop - R Post. Corner",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/41.PNG"
+    "img": "./img/41.PNG"
   },
   {
-    "character": "42 Prop - R Post. Corner",
-    "part": "mesosoma", 
+    "character": "42 Prop - R Ant. Gland Opening",
+    "part": "mesosoma",
     "index": 3,
-    "img":"./img/42.PNG"
-  },
-  {
-    "character": "43 Prop - R Ant. Gland Opening",
-    "part": "mesosoma", 
-    "index": 3,
-    "img":"./img/43.PNG"
-  },
-  {
-    "character": "44 Prop - R Post. Gland Opening",
-    "part": "mesosoma", 
-    "index": 3,
-    "img":"./img/44.PNG"
+    "img": "./img/42.PNG"
   }
 ];
